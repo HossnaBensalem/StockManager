e49600446fac37966ee200f833df17f689c8e6c1
HossnaBensalem <hossnabensalem2@gmail.com>
2024-12-21 16:52:40 +0100

    ajouter les

--- stock_manager.js
+++ stock_manager.js
@@ -1,5 +1,51 @@
-
+const fs = require('fs');
+const Inventory = require('./inventory');
 const prompt = require('prompt-sync')();
-const Inventory = require("./Inventory");
-const Inventory = new Inventory ();
 
+const inventory = new Inventory();
+
+function displayMenu() {
+    console.log("\n1. Ajouter un produit");
+    console.log("2. Afficher tous les produits");
+    console.log("3. Mettre à jour un produit");
+    console.log("4. Supprimer un produit");
+    console.log("5. Quitter");
+}
+
+function stockManager() {
+    let choice;
+    do {
+        try {
+            displayMenu();
+            choice = prompt("Choisissez une option: ");
+            switch (choice) {
+                case '1':
+                    inventory.addProduct();
+                    break;
+
+                case '2':
+                    inventory.listProducts();
+                    break;
+
+                case '3':
+                    inventory.updateProduct();
+                    break;
+
+                case '4':
+                    inventory.deleteProduct();
+                    break;
+
+                case '5':
+                    console.log("Au revoir !");
+                    break;
+
+                default:
+                    console.log("Option invalide. Essayez encore.");
+            }
+        } catch (error) {
+            console.error("Une erreur est survenue dans le programme principal : ", error.message);
+        }
+    } while (choice !== '5');
+}
+
+stockManager();
